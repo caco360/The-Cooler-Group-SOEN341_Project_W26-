0970cf7e7d76109c724cec470000969db59ac106
Besher Alsabbagh <sabbaghbesher8@gmail.com>
2026-02-25 15:15:12 -0500

    doing the filter by time function
    finishing US13 and adding the filter by time functionality to the recipes HTML

--- Backend/Server/supabaseClient.js
+++ Backend/Server/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = "https://aiykmnzqhyztjslbcgbo.supabase.co"
+const supabaseKey = "sb_publishable_8e6VessR9bNZzvinQT4OPA_9y8ySWbK"
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing SUPABASE_URL or SUPABASE_ANON_KEY in .env");
