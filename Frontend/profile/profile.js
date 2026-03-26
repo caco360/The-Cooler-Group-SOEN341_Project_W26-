8c3142f9862ab61501a44d455e6a516012920cba
Nick <nchamoun@hotmail.com>
2026-03-25 21:26:48 -0400

    fixed some biometrics stuff, all that we need to do is do the backend stuff with this + make it so the biometric data is saved

--- Frontend/profile/profile.js
+++ Frontend/profile/profile.js
@@ -295,6 +295,7 @@ async function handleDeleteAccount() {
 /* ============================
    BMI Calculation Logic
 =============================== */
+
 document.addEventListener("DOMContentLoaded", () => {
   const calculateBtn = document.getElementById("calculateBMIBtn");
   const ageInput = document.getElementById("ageInput");
