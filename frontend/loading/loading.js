244102edf9b47f7fc48a0fe341a74e5b3cdc996a
jerrish-david <jerrish.github@gmail.com>
2026-04-13 20:08:57 -0400

    optimizing user exp by ginving longer loadint time to ensure next page loaded

--- frontend/loading/loading.js
+++ frontend/loading/loading.js
@@ -6,7 +6,7 @@ const loadingMessage = document.getElementById("loadingMessage");
 if (loadingMessage && message) {
   loadingMessage.textContent = message;
 }
-
+//optimizing user experience by giving a longer loading time to ensure the next page is fully loaded before redirecting
 window.setTimeout(() => {
   window.location.href = next;
 }, 900);
